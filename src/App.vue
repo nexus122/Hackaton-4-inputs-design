67613cf6d54018b1eddf3c8d589695483786a3fb
nexus122 <juan-pablo122@hotmail.es>
2022-05-17 18:31:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import InputComponent from "./components/InputComponent.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      config: {
+        type: "default",
+        placeholder: "Your name",
+      },
+    };
+  },
+};
+</script>
+
+
+<template>
+  <!-- Defalut -->
+  <p>Default</p>
+  <InputComponent :config="config" :type="'default'" />
+
+  <!-- Hover -->
+  <p>Hover</p>
+  <InputComponent :config="config" :type="'hover'" />
+
+  <!-- Filled -->
+  <p>Filled</p>
+  <InputComponent :config="config" :type="'filled'" :value="'Michael Browk'" />
+
+  <!-- Disabled -->
+  <p>Disabled</p>
+  <InputComponent :config="config" :type="'disabled'" :disabled="true" />
+
+  <!-- Disabled Filled -->
+  <p>Disabled Filled</p>
+  <InputComponent
+    :config="config"
+    :type="'disabledFilled'"
+    :disabled="true"
+    :value="'Michael Browk'"
+  />
+
+  <!-- ReadOnly -->
+  <p>ReadOnly</p>
+  <InputComponent
+    :config="config"
+    :type="'ReadOnly'"
+    :readOnly="true"
+    :value="'Michael Browk'"
+  />
+
+  <!-- Error -->
+  <p>Error</p>
+  <InputComponent
+    :config="config"
+    :type="'error'"    
+    :backgroundColor="'#f5c6cb'"
+  />
+
+  <!-- Error Filled -->
+    <p>Error Filled</p>
+    <InputComponent
+      :config="config"
+      :type="'errorFilled'"          
+      :backgroundColor="'#f5c6cb'"
+    />
+
+  <!-- Success -->
+  <p>Success</p>
+  <InputComponent :config="config" :value="'Michael Browk'" :type="'success'" :backgroundColor="'#CBECD9'" :icon='`<i class="fa-solid fa-check"></i>`'/>
+
+  <!-- Description -->
+  <p>Description</p>
+  <InputComponent :config="config" :type="'description'" :description="'Something to say to the user'" />
+
+  <!-- Autofill -->
+  <p>Autofill</p>
+  <InputComponent :config="config" :type="'Autofill'" :backgroundColor="'#FFF5C0'" :icon='`<i class="fa-solid fa-check"></i>`' :value="'Michael Browk'"/>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+body {
+  font-family: "Roboto", sans-serif;
+}
+</style>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="input">
+    <label
+      v-if="
+        ['success','Autofill','errorFilled', 'filled', 'disabledFilled', 'ReadOnly'].includes(type)
+      "
+      class="miniText"
+      for="input"
+      >{{ config.placeholder }}</label
+    >
+    <input
+      name="input"
+      type="text"
+      :value="value"
+      :placeholder="config.placeholder"
+      :class="config.type"
+      @focus="focus"
+      :disabled="disabled"
+      :readOnly="readOnly"
+      :style="{ background: backgroundColor }"
+    />
+    <p class="icon" v-html="icon"></p>
+    <p class="error_msg" v-if="['errorFilled', 'error'].includes(type)">
+      Error - Something
+    </p>
+    <p>{{description}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    backgroundColor: {
+      type: String,
+      default: "#fff",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    config: {
+      type: Object,
+      default: () => {
+        return {
+          type: "default",
+          placeholder: "Your name",
+        };
+      },
+    },
+  },
+  methods: {
+    focus: function () {},
+  },
+};
+</script>
+
+<style scoped>
+input {
+  border: 0px solid #ccc;
+  padding: 16px;
+  box-sizing: border-box;
+  min-width: 322px;
+  min-height: 46px;
+
+  /* Border  */
+  border-radius: 4px;
+
+  /* Tipografia */
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+
+  /*  */
+  box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.04),
+    0px 4px 16px rgba(51, 51, 51, 0.08);
+}
+
+.default {
+  border: 1px solid #ccc;
+}
+
+.miniText {
+  position: absolute;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 20px;
+  padding-left: 16px;
+  padding-top: 2px;
+
+  /* Color */
+  color: rgba(17, 17, 17, 0.48);
+}
+.error_msg {
+  color: rgba(219, 82, 78, 1);
+}
+.icon {
+  position: relative;
+  top: -55px;
+  left: 290px;
+  color: #27AE60;
+}
+</style>
